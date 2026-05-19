a8b7adcc3f1f9c87d69d580919fc2ae2460287b8
marcosvalenzz <malkirrin@gmail.com>
2026-05-19 20:39:11 +0200

    cambio

--- app/src/main/java/Modelo/Niveles.java
+++ app/src/main/java/Modelo/Niveles.java
@@ -5,4 +5,45 @@ public class Niveles {
     private String idNiveles;
     private String seccion;
     private int numeroNivel;
+
+    public Niveles() {}
+
+    public Niveles(String idNiveles, String seccion, int numeroNivel) {
+        this.idNiveles = idNiveles;
+        this.seccion = seccion;
+        this.numeroNivel = numeroNivel;
+    }
+
+    public String getIdNiveles() {
+        return idNiveles;
+    }
+
+    public void setIdNiveles(String idNiveles) {
+        this.idNiveles = idNiveles;
+    }
+
+    public String getSeccion() {
+        return seccion;
+    }
+
+    public void setSeccion(String seccion) {
+        this.seccion = seccion;
+    }
+
+    public int getNumeroNivel() {
+        return numeroNivel;
+    }
+
+    public void setNumeroNivel(int numeroNivel) {
+        this.numeroNivel = numeroNivel;
+    }
+
+    @Override
+    public String toString() {
+        return "Niveles{" +
+                "idNiveles='" + idNiveles + '\'' +
+                ", seccion='" + seccion + '\'' +
+                ", numeroNivel=" + numeroNivel +
+                '}';
+    }
 }
