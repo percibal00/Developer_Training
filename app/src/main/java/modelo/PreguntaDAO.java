db0b7596dc8ddb4b3c85191d734f4fdeac6776c6
percibal00 <alexlopezjimenez06@gmail.com>
2026-05-25 00:07:27 +0200

    main

--- app/src/main/java/modelo/PreguntaDAO.java
+++ app/src/main/java/modelo/PreguntaDAO.java
@@ -44,26 +44,4 @@ public class PreguntaDAO {
         }
         return lista;
     }
-
-    public void insertarPregunta(Pregunta p) {
-        new Thread(() -> {
-            try {
-                Connection conn = ConexionMySQL.conectar();
-                if (conn == null) return;
-
-                String sql = "INSERT INTO preguntas (pregunta, respuesta, tipo) VALUES (?, ?, ?)";
-                try (PreparedStatement ps = conn.prepareStatement(sql)) {
-                    ps.setString(1, p.getPregunta());
-                    ps.setString(2, p.getRespuesta());
-                    ps.setString(3, p.getTipo());
-
-                    ps.executeUpdate();
-                    System.out.println("INSERTADO");
-                }
-                conn.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }).start();
-    }
 }
